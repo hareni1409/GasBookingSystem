ee18a9c9abdbf268bdc63d4c741c86e357ea4660
Hareni M <84172987+hareni1409@users.noreply.github.com>
2022-02-08 12:50:06 +0530

    Delete GasBookingSystem directory

--- GasBookingSystem/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//database connection code
-$con=new mysqli("localhost","root","Healing@5","gas_booking");
-if($con->connect_error){
-    die("Error in connection:".$con->connect_error);
-}
-?>
