8bb10bd780590ee0a86f9d4e75e662e18fafbb84
Hareni M <84172987+hareni1409@users.noreply.github.com>
2022-02-08 12:51:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //database connection code
-$con=new mysqli("localhost","root","Healing@5","gas_booking");
+$con=new mysqli("localhost","root","***","gas_booking");
 if($con->connect_error){
     die("Error in connection:".$con->connect_error);
 }
